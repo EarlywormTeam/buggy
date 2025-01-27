1f15f546197028dbcfbbeda40d9e9e740d014981
Chris Wood <christopherhwood@gmail.com>
2025-01-26 18:38:51 -0800

    Fix subscribe api

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
+  const { name } = req.body;
+  if (typeof name === 'string' && name.toLowerCase().includes('error')) {
+    return res.status(500).json({ message: 'Error!' })
+  }
+
   // Pretend to do something with name, email, message
   // In a real scenario, you'd store them or send an email.
   res.status(200).json({ message: 'Subscribed successfully!' })
